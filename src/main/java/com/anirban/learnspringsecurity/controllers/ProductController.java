7c13022c1273dd6f0ad71b84cc60f15cc9dd828c
Anirban <ad653157@gmail.com>
2023-10-17 10:24:09 +0530

    My first commit without the spring security, a simple e-commerce catalog with hard coded products

---
+++ src/main/java/com/anirban/learnspringsecurity/controllers/ProductController.java
@@ -0,0 +1,42 @@
+package com.anirban.learnspringsecurity.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anirban.learnspringsecurity.models.Product;
+import com.anirban.learnspringsecurity.services.ProductService;
+
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+	
+	
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+  
+
+    @GetMapping("/all")
+   
+    public List<Product> getAllTheProducts() {
+        return service.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProductById(id);
+    }
+
+}
---
+++ src/main/java/com/anirban/learnspringsecurity/services/ProductService.java
@@ -0,0 +1,46 @@
+package com.anirban.learnspringsecurity.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anirban.learnspringsecurity.models.Product;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class ProductService {
+	
+
+    List<Product> productList = new ArrayList<>();
+
+   
+
+    @PostConstruct
+    public void loadProductsFromDB() {
+       for(int i=1;i<=50;i++) {
+    	   Random random = new Random();
+    	   Product pd = new Product();
+    	   pd.setProductId(i);
+    	   pd.setName("Product "+i);
+    	   pd.setQty(random.nextInt(10) + 1);
+    	   pd.setPrice(Math.round(random.nextDouble(100,200)*100.0)/100.0 );
+    	   productList.add(pd);
+       }
+    }
+
+
+    public List<Product> getAllProducts() {
+        return productList;
+    }
+    
+    public Product getProductById(int id) {
+    	return productList.get(id);
+    }
+
+}
